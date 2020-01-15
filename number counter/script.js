860ab3b0c89ab1c973300c16c6c82ad82081252a
elddiriddle <elddiriddle@gmail.com>
2020-01-14 22:59:19 -0500

    Add files via upload

---
+++ number counter/script.js
@@ -0,0 +1,10 @@
+let count = 0;
+function incr(){        
+    count += 1;
+    document.getElementById("counter").value = count;  
+}
+
+function decr(){        
+    count -= 1;
+    document.getElementById("counter").value = count;  
+}
